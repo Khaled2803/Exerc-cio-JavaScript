9fdb5f4842c0508208207d313f1eed2b7c5977cb
khaled <khaledantunino2016@gmail.com>
2023-03-14 16:23:40 -0300

    Exercício básico de JavaScript

---
+++ scripts.js
@@ -0,0 +1,135 @@
+// Object
+/*
+const person = {
+    name: 'John',
+    age: 30,
+    weight: 88.6,
+    isAdmin: true
+}
+
+console.log(`${person.name} tem ${person.age} anos`) 
+
+
+//Array
+
+const animals = [
+    'lion',
+    'Monkey',
+    {
+        name: 'Cat',
+        age: 3
+    }
+]
+
+// acessar valores dentro do Array
+console.log(animals[2].name)
+*/
+
+
+
+
+
+
+
+/* 1. Declare uma variável de nome weight */
+// let weight
+
+/* 2. Que tipo de dado é a variável acima? */
+// console.log(typeof weight)
+
+/*
+    3. Declare uma variável e atribua valores para cada um dos dados:
+        * name: String
+        * age: Number (integer)
+        * stars: Number (float)
+        * isSubscribed: Boolean
+*/
+
+// let name = 'José'
+// let age = '45'
+// let stars = '12.5'
+// let isSubscribed = 'true'
+
+/*
+    4. A variável student abaixo é de que tipo de dado?
+
+    4.1 Atribua a ela as mesmas propriedades e valores do exercício 3.
+
+    4.2 Mostre no console a seguinte mensagem:
+
+        <name> de idade <age> pesa <weight> kg.
+
+        Atenção, sbstitua <name> <age> e <weight> pelos valores de cada propriedade do objeto
+*/
+
+/*
+let student = {
+    name:"Maria",
+    age: 45,
+    weight: 65.5,
+    isSubscribed: true,
+};
+*/
+
+//console.log(`${student.name} de idade ${student.age} pesa ${student.weight} kg`)
+
+
+/*
+    5. Declare uma variável do tipo Array, de nome students e atribua a ela nenhum valor, ou seja, somente o Array vazio
+*/
+
+// let students = []
+
+/*
+    6. Reatribua valor para a variável acima, colocando dentro dela o objeto student da questão 4. (Não copiar e colar o objeto, mas usar o objeto criado e inserir ele no Array)
+*/
+
+/*
+students = [
+    student
+]
+*/
+
+// console.log(students)
+
+/*
+    7. Coloque no console o valor da posição zero do Array acima
+*/
+
+// console.log(students[0])
+
+/*
+    8. Crie um novo student e coloque na posição 1 do Array students
+*/
+
+/*
+const John = {
+    name: "John",
+    age: 23,
+    weight: 67.8,
+    isSubscribed: true,
+}
+*/
+
+/*
+students = [
+    student,
+    John
+]
+*/
+
+// students[1] = John
+// ou
+// console.log(students)
+
+/*
+    9. Sem rodar o código reponda qual é a resposta do código abaixo e por que? Após sua resposta, rode o código e veja se você acertou.
+
+    console.log(a)
+    var a = 1
+*/
+
+// console.log(a)
+// var a = 1
+
+// R: Underfild.
